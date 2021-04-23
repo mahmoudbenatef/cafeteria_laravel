98a6e07a58eba6da64ea186891dc50e03fe2e7b7
HatemHashem <hatemsayed001@gmail.com>
2021-04-23 19:53:33 +0200

    link user to his social account and generate sanctum token

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\UserResource;
+use App\Models\SocialAccount;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -21,6 +22,38 @@ class SocialAuthController extends Controller
     {
 
         $user = Socialite::driver($provider)->stateless()->user();
-        dd($user);
+        $appUser=User::whereEmail($user->email)->first();
+        // dd( $user);
+        if(!$appUser){
+            $socialUser = new User();               
+            $socialUser->name = $user->nickname;
+            $socialUser->email = $user->email;
+            $socialUser->password = "12345";
+            $socialUser->ext="default";
+            $socialUser->room_id=1;
+            $socialUser->photo = $user->avatar;
+            // dd( $user->avatar);
+            $socialUser->save();
+            $socialAcc=SocialAccount::create([
+                'provider'=>$provider,
+                'provider_user_id'=>$user->id,
+                'user_id'=>$socialUser->id
+            ]);
+
+        }else{
+            //already registered with that email 
+            $socialAcc=$appUser->socialAccounts()->where('provider',$provider);
+            // dd("found");
+            if(!$socialAcc){
+                $socialAcc=SocialAccount::create([
+                    'provider'=>$provider,
+                    'provider_user_id'=>$user->id,
+                    'user_id'=>$appUser->id
+                ]);
+            }
+        }
+         $appUser->token = $appUser->createToken($appUser->email)->plainTextToken;
+        return response()->json(['status' => "success", "message" => "user logged in successfully", "user" => new UserResource($appUser)], 200);
+       
     }
 }
