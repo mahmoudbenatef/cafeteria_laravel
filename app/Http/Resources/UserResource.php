a9366f96c31651848e2d3cbacfacb7408bbbf7ad
Atef <mahmoudbenatef@gmail.com>
2021-04-27 23:59:17 +0200

    handle user image

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -21,6 +21,7 @@ class UserResource extends JsonResource
             'ext'=> $this->ext,
             'token'=>$this->token,
             'isAdmin'=>$this->isAdmin,
+            'photo'=>$this->photo,
         ];
     }
 }
